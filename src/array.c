7cdc7a7b26120d7691a67a1e21559f440c66453f
chauncey <2268136725@qq.com>
2022-05-06 19:56:17 +0800

    update

--- src/array.c
+++ src/array.c
@@ -3,3 +3,28 @@
 //
 
 #include "array.h"
+#include <stdlib.h>
+
+Node *create() {
+    Node *L;
+    L = (Node *) malloc(sizeof(Node) * MAX_LEN);
+    for (int i = 0; i < MAX_LEN; i++) {
+        L++->id = -1;
+    }
+    return L;
+}
+
+int len(Node *L) {
+    for (int i = 0; i < MAX_LEN; i++) {
+        if (L[i].id == -1) {
+            return i;
+        }
+    }
+    return MAX_LEN;
+}
+
+status insert(Node *L, Node node, unsigned index) {
+    if (index >= len(L)) {
+        exit(-1);
+    }
+}
--- src/array.h
+++ src/array.h
@@ -7,16 +7,20 @@
 
 #include "error.h"
 
+#define MAX_LEN 30
+
 // 定义数据元素
 typedef struct {
     int id;
     char name[30];
-    char sex; // man->m, woman->w
+    char sex;// man->m, woman->w
     unsigned int age;
 } Node;
 
-status insert(Node *L, Node node, unsigned location);
-status delete(Node *L, unsigned location);
+Node *create();
+status insert(Node *L, Node node, unsigned index);
+status delete (Node *L, unsigned index);
+int len(Node *L);
 
 
 #endif//DATASTRUCTURE_ARRAY_H
